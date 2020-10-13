a9265d5ac9ed69076ddf4b93a034d6150ed3040d
Joril <mypopcs@163.com>
2020-10-13 16:58:00 +0800

    修正白名单单词包含特殊字符串的bug

--- upperCase/uppercase.js
+++ upperCase/uppercase.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 //专有名词数组
 var settings = JSON.parse(fs.readFileSync('whiteWords.json', 'utf8'))
 var whiteWordsGrop = settings.whiteWords
+var specilStrs = settings.specilStrs
 
 //定义首字母大写公共函数
 function pubUpperCase(z){
@@ -19,21 +20,43 @@ function pubUpperCase(z){
     }
     return z
 }
-
+//白名单中包含特殊字符串
+function uploadWhiteWords(whiteWordsObj, specilRule){
+    for (s = 0; s < specilRule.length; s++){
+        var specilStr = '/[\\' + specilRule[s] + ']/g'
+        var specilRegex = eval(specilStr)
+        if(whiteWordsObj.match(specilRegex)){
+            whiteWordsObj = whiteWordsObj.replace(specilRegex, '\\' + specilRule[s])
+            return whiteWordsObj
+        }
+    }
+}
 //恢复白名单大写
 function whiteList(s, whiteWordsList){
+    //如果
     if(typeof s == 'object'){
         for(i = 0; i < s.length; i++){
             for (x = 0; x < whiteWordsList.length; x++){
+                //判断白名单字符串中是否包含/ - :，如果包含执行白名单字符串排除
+                if (whiteWordsList[x].match(/[\/|\-|\:]/g)){
+                    var whiteWord = uploadWhiteWords(whiteWordsList[x], specilStrs)
+                } else {
+                    var whiteWord = whiteWordsList[x]
+                }
                 //根据白名单生成正则
-                var whiteWordsRule = '/\\b' + whiteWordsList[x] + '\\b/ig'
+                var whiteWordsRule = '/\\b' + whiteWord + '\\b/ig'
                 //eval可以计算某个字符串并执行其中的js代码，比如用来执行正则替换
                 s[i] = s[i].replace(eval(whiteWordsRule), whiteWordsList[x])
             }
         }
     } else {
         for (x = 0; x < whiteWordsList.length; x++){
-            var whiteWordsRule = '/\\b' + whiteWordsList[x] + '\\b/ig'
+            if (whiteWordsList[x].match(/[\/|\-|\:]/g)){
+                var whiteWord = uploadWhiteWords(whiteWordsList[x], specilStrs)
+            } else {
+                var whiteWord = whiteWordsList[x]
+            }
+            var whiteWordsRule = '/\\b' + whiteWord + '\\b/ig'
             s = s.replace(eval(whiteWordsRule), whiteWordsList[x])
           }
     }
@@ -49,6 +72,9 @@ function getValues(file) {
             getValues(objValues)
         } else {
             //满足. ? !就分割句子
+            // \s匹配所有空白符，包括换行；.匹配除换行符之外任何单字符；+匹配前面的子表达式一次或多次；?匹配前面的子表达式零次或一次；$匹配输入字符串的结尾位置
+            //()标记一个子表达式的开始和结束位置；[]标记一个中括号表达式的开始与结束
+            // \b 匹配一个单词边界，即字与空格间的位置
             if(objValues.match(/(.+?[\.\?\!](\s|$))/g)){
                 var splitList = file[values] = objValues.split(/(.+?[\.\?\!](\s|$))/g);
                 //执行大写
